1da7a8be293c1c8a26addf3718c4a6df16472f6a
hordiienko.ah <hordiienko.ah@pg.com>
2026-01-19 13:47:52 +0200

    dockerSupport

---
+++ Catalog.Identity/Migrations/AuthDb/20260119010333_InitialIdentityMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Identity.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class InitialIdentityMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
